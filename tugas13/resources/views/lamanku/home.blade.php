ccacd306d66c0b26cf4cfaf120622bd6939336fe
raviirfansyahabdullah <raviirfansyahbadullah@gmail.com>
2023-04-13 21:48:36 +0700

    Table Raviirfansyahabdullah

---
+++ tugas13/resources/views/lamanku/home.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.master')
+@section('title')
+Halaman Utama
+@endsection
+@section('subtitle')
+Halaman Utama
+@endsection
+
+@section('content')
+    
+            <h1>Garuda Cyber Institute</h1>
+            <h3>Jadilah Programmer Handal Bersama GC-INS</h3>
+            <p>Grow Together With Garuda Cyber Institute</p>
+
+
+        <div class="content">
+            
+            <div class="benefit">
+                <h4>Syarat dan Ketentuan</h4>
+                <ul>
+                    <li>Tamatan SMA/SMK</li>
+                    <li>Tamatan Perguruan Tinggi</li>
+                    <li>Pekerja IT</li>
+                    <li>Freelancer</li>
+                </ul>
+            </div>
+
+            <div class="cara_join">
+                <h4>Cara Bergabung</h4>
+                <ol>
+                    <li>Kunjungi Website GC-INS</li>
+                    <li>Register <a href="/register">Form Sign Up</a></li>
+                    <li>Lakukan Pembayaran</li>
+                </ol>
+            </div>
+        </div>
+@endsection
