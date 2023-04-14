72c432f2798e955e668710c611aa65be3e682caa
raviirfansyahabdullah <raviirfansyahbadullah@gmail.com>
2023-04-14 21:09:17 +0700

    MIGRATION raviirfansyahabdullah

---
+++ tugas14/database/migrations/2023_04_14_131335_create_film_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('film', function (Blueprint $table) {
+            $table->bigIncrements('id_film');
+            $table->string('judul');
+            $table->string('ringkasan');
+            $table->string('tahun');
+            $table->string('poster');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('film');
+    }
+};
